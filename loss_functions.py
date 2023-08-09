537fdcfc8049cda259a21fb13a7815fb1d99a4d9
fabiocfabini <fabiolucas.carneiro@gmail.com>
2023-08-09 15:35:39 +0100

    commit more trash

--- loss_functions.py
+++ loss_functions.py
@@ -1,7 +1,9 @@
 import torch
 
 def mse_loss(input: torch.Tensor, target: torch.Tensor, weights: torch.Tensor) -> torch.Tensor:
-    return torch.einsum('ij,i->ij', (input - target)**2, weights)
+    return torch.pow(input - target, 2).sum(dim=1) * weights
 
 def mae_loss(input: torch.Tensor, target: torch.Tensor, weights: torch.Tensor) -> torch.Tensor:
-    return torch.einsum('ij,i->ij', torch.abs(input - target), weights)
+    """return torch.abs(input - target) * weights
+    """
+    return torch.abs(input - target).sum(dim=1) * weights
